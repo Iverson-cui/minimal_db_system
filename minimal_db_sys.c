b86484d628fe6a34d20f3272451e149a151a5a72
Iverson-cui <yuboiverson@gmail.com>
2025-03-27 23:18:06 +0800

    modified code before database _write

--- minimal_db_sys.c
+++ minimal_db_sys.c
@@ -52,26 +52,52 @@ void Address_print(struct Address *addr)
 // file contains the whole content of rows, but in db struct, rows is just a pointer pointing to the contents.
 void Database_load(struct Connection *conn)
 {
+    // flags to see if writing has been properly handled.
+    int flag_load_MACRO;
+    int flag_load_ints[conn->db->MAX_ROWS];
+    int flag_load_strs[conn->db->MAX_ROWS];
+    // In file we save the concrete address data. But when loaded to the db, we have to first malloc space then use a pointer.
+    // Every time we open and load a db, we have to malloc a new space to keep the data in file.
+    // Do we have to have an end flag for non-char array?
+    conn->db->rows = malloc((sizeof(struct Address)) * conn->db->MAX_ROWS);
+    // Notice that only the pointer addrpt is assigned to conn->db->rows. The data in file is not assigned to conn->db->rows.
     // write data from to db pointer points to, from the file.
-    // first write 2 ints. Notice that the file pointer automatically update its position.
-    int flag_load_ints = fread(conn->db, sizeof(int), 2, conn->file);
+    // first write 2 ints, MAX_DATA and MAX_ROW. Notice that the file pointer automatically update its position.
+    flag_load_MACRO = fread(conn->db, sizeof(int), 2, conn->file);
     // if successfully it should return 2.
-    // assign space of MAX_ROWS of addresses
-    struct Address *addrpt = malloc(sizeof(struct Address) * conn->db->MAX_ROWS);
-    // write MAX_ROWS address to the addrpt, then assign addrpt to conn->db->rows. Now in the db there is a pointer.
-    int flag_load_addr = fread(addrpt, sizeof(struct Address), conn->db->MAX_ROWS, conn->file);
-    // if successfully it should return MAX_ROWS.
-    conn->db->rows = addrpt;
-    if ((flag_load_ints * flag_load_addr) == 0)
+    // write data into where rows point to.
+    for (int i = 0; i < conn->db->MAX_ROWS; i++)
     {
-        die("Failed to load database", conn);
+        /*
+        handle writing of different granularities with the help of type casting and char *
+        rows is an address pointer, so it increments with the size of an address.
+        If we want to write data within the address, we need a pointer that is of finer granularity, like char * and do type casting to write other type to the address
+         */
+
+        // // temp is a char * pointer with the granularity of 1 byte pointing at the start of this address
+        // char *temp = (char *)((conn->db->rows) + i);
+        // // convert char * to int * to write int id and int set
+        // flag_load_ints[i]= fread((int *)temp,sizeof(int), 2, conn->file);
+        flag_load_ints[i] = (fread(&(conn->db->rows[i].id), sizeof(int), 1, conn->file) &
+                             fread(&(conn->db->rows[i].set), sizeof(int), 1, conn->file));
+        // flag_load_ints[i] = fread(&(conn->db->rows[i].set), sizeof(int), 1, conn->file);
+        conn->db->rows[i].name = malloc(conn->db->MAX_DATA);
+        conn->db->rows[i].email = malloc(conn->db->MAX_DATA);
+        flag_load_strs[i] = (fread(conn->db->rows[i].name, sizeof(char), conn->db->MAX_DATA, conn->file)) & (fread(conn->db->rows[i].email, sizeof(char), conn->db->MAX_DATA, conn->file));
+        // if successfully it should return MAX_ROWS.
+        if ((flag_load_ints[i] * flag_load_strs[i]) == 0)
+        {
+            die("Failed to load database", conn);
+        }
     }
 }
-// when you have just a single file(char *), Database_open gives you the connection, Database_load help you update the db.
+// when you have just a single file(char *), Database_open gives you the connection, while Database_load help you update the db.
+// In Database_load, based on the db pointer, we allocate space for MAX_DATA, MAX_ROWS and rows.
 struct Connection *Database_open(const char *filename, char mode)
 {
     struct Connection *conn = malloc(sizeof(struct Connection));
     conn->db = malloc(sizeof(struct Database));
+    // After searching the web, I found that the malloc function does not initialize the memory it allocates. So we have to initialize the memory ourselves. But we have access to the member elements now using ->.
     if (!conn)
     {
         die("Memory error", conn);
@@ -109,6 +135,20 @@ void database_close(struct Connection *conn)
         {
             if (conn->db->rows)
             {
+                // first free all the name and email pointers in the rows struct.
+                for (int i = 0; i < conn->db->MAX_ROWS; i++)
+                {
+                    // free the name and email pointer in the rows struct.
+                    if (conn->db->rows[i].name)
+                    {
+                        free(conn->db->rows[i].name);
+                    }
+                    if (conn->db->rows[i].email)
+                    {
+                        free(conn->db->rows[i].email);
+                    }
+                }
+                // free rows pointer itself
                 free(conn->db->rows);
             }
             free(conn->db);
@@ -155,13 +195,12 @@ void Database_create(struct Connection *conn, int MAX_DATA, int MAX_ROWS)
     // initialize rows pointer in the db struct.
     // for a pointer, first allocate space, then assign concrete data type to the space.
     // allocate space first
-    struct Address *temp_addr = malloc(sizeof(struct Address) * MAX_ROWS);
+    conn->db->rows = malloc(sizeof(struct Address) * MAX_ROWS);
     for (int i = 0; i < conn->db->MAX_ROWS; i++)
     {
         // assign to the space
-        temp_addr[i] = (struct Address){.id = i, .set = 0};
+        conn->db->rows[i] = (struct Address){.id = i, .set = 0};
     }
-    conn->db->rows = temp_addr;
 }
 // set the name and email of a specific row in the conn database
 void Database_set(struct Connection *conn, int id, const char *name, const char *email)
